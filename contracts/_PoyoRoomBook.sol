f9135f4d01a4582f92a3460cf6e0ffb6c5beb570
sanjipun <iamsanjipun@gmail.com>
2022-01-13 21:37:08 +0545

    update contract

--- contracts/_PoyoRoomBook.sol
+++ contracts/_PoyoRoomBook.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // The sample hotel and vending Solidity smart contract below allows one to rent a hotel room. It allows someone to make a payment for a room if the room is vacant. After payment is made to the contract the funds are sent to the owner. This smart contract can be expanded to unlock the door or dispense a key code after payment is made.
 // Think of this contract like a vending machine. You input funds, validations pass, and you get something in return. It is the same concept as a gumball machine. 
 
@@ -10,24 +10,30 @@ contract PoyoRoomBooking {
     using SafeMath for uint;
     uint public minimumAmount = 0;
     uint public totalRooms = 5;
-    address public owner;
+    address payable public owner;
+
     AggregatorV3Interface internal priceFeed;
 
     constructor() {
         priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
 
-    mapping(address =>string[]) private customers;
+    mapping(string => mapping(uint => roomStatus) ) public customers;
+    mapping(uint => string) public motelBranches;
 
     struct roomStatus {
-        bool isVacant;
         uint roomNumber;
-        uint timeLeft;
-        address usedBy;
+        uint time;
+        string usedBy;
+        address bookingAddress;
+        string branch;
     }
 
-    roomStatus[] public roomsStatus;
+    modifier onlyOwner() {
+        require(msg.sender == owner,"Only owner can do this task!");
+        _;
+    }
 
     function setMinimumAmount(uint _amount) public {
         minimumAmount = _amount * 10 ** 18;
@@ -44,8 +50,31 @@ contract PoyoRoomBooking {
         return tokenPriceInUsd;
     }
 
-    modifier minimumAmountModifier(uint _amount) {
-        require(getConversionRate(_amount) > minimumAmount,"The Amount must be greater than minimum amount!");
-        _;
+    function checkIn(uint _roomNumber,string calldata _branchName,uint _time, string calldata _usedBy) public payable{
+        // require(getConversionRate(msg.value) > minimumAmount,"The Amount must be greater than minimum amount!");?
+        customers[_branchName][_roomNumber]=roomStatus({
+            roomNumber: _roomNumber,
+            time: block.timestamp + _time,
+            usedBy: _usedBy,
+            bookingAddress: msg.sender,
+            branch: _branchName
+        });
+    }
+
+    function checkOut(string calldata _branchName,uint _roomNumber) public {
+        delete customers[_branchName][_roomNumber];  
+    } 
+
+    function getRoomStatus(string calldata _branchName,uint _roomNumber) public view returns(roomStatus memory) {
+        return customers[_branchName][_roomNumber];
+    }
+
+
+    function addBranch(uint _id,string calldata _branchName) public onlyOwner{
+        motelBranches[_id]=_branchName;
+    }
+
+    function deleteBranch(uint _id) public onlyOwner {
+        delete motelBranches[_id];
     }
 }
