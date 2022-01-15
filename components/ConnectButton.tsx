5eb67c2e500fc2e784669cef59e2f8286ac09353
sanjipun <iamsanjipun@gmail.com>
2022-01-15 20:43:25 +0545

    day 5: update ui

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface ConnectButtonProps {}
 
 const ConnectButton: React.FC<ConnectButtonProps> = () => {
-  return <button className="bg-pink-600 p-2 rounded-md">Connect Wallet</button>;
+  return <button className="bg-pink-600 p-2 rounded-md text-white">Connect Wallet</button>;
 };
 
 export default ConnectButton;
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -5,33 +5,33 @@ interface LandingPageProps {}
 const LandingPage: React.FC<LandingPageProps> = () => {
   return (
     <div className="mx-auto">
-      <div className="container mx-auto h-full pt-32 w-full pb-20 px-20">
-        <h1 className="font-black text-8xl text-white">
+      <div className="container mx-auto h-full pt-64 w-full pb-20 px-20">
+        <h1 className="font-black text-8xl text-black">
           Book <span className="text-pink-600">Rooms</span>
         </h1>
-        <h1 className="text-white font-black text-8xl mt-10">
+        <h1 className="text-black font-black text-8xl mt-10">
           At <span className="text-pink-600">PoYo.</span>
         </h1>
         <div className="mt-32 max-w-7xl mx-auto border p-3 flex justify-around text-black">
-          <select className="p-2 w-full mx-2">
+          <select className="p-2 w-full mx-2 bg-black text-white">
             <option value="" disabled selected>
               Select Branch
             </option>
             <option value="sad">sad</option>
           </select>
-          <select className="p-2 w-full mx-2">
+          <select className="p-2 w-full mx-2 bg-black text-white">
             <option value="" disabled selected>
               Select Room Number
             </option>
             <option value="sad">sad</option>
           </select>
-          <select className="p-2 w-full mx-2">
+          <select className="p-2 w-full mx-2 bg-black text-white">
             <option value="" disabled selected>
               Days of Stay
             </option>
             <option value="sad">sad</option>
           </select>
-          <input placeholder="Name" className="p-2 w-full mx-2"></input>
+          <input placeholder="Name" className="p-2 w-full mx-2 bg-black text-white"></input>
           <button className="p-2 bg-pink-600 w-full text-white">Book Room</button>
         </div>
       </div>
