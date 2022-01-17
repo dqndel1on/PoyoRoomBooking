1593d92e8b7d4d66f67ab027099c26949d417994
sanjipun <iamsanjipun@gmail.com>
2022-01-17 22:58:41 +0545

    day 7: update admin page file

---
+++ pages/admin.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import React from 'react';
+import AddBranch from '../components/AddBranch';
+import BranchListAdmin from '../components/BranchListAdmin';
+import UpdateBranch from '../components/UpdateBranch';
+
+interface adminProps {}
+
+const admin: React.FC<adminProps> = () => {
+  return (
+    <div className="container mx-auto px-20">
+      <Head>
+        <title>Add/Delete/Update Branch | Poyo</title>
+      </Head>
+      <AddBranch />
+      <UpdateBranch />
+      <BranchListAdmin />
+    </div>
+  );
+};
+
+export default admin;
