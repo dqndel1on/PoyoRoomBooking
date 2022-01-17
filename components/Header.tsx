8d626049b94da50e23e51284de04b0f8ab8971ad
sanjipun <iamsanjipun@gmail.com>
2022-01-17 22:58:29 +0545

    day 7: update header file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import usePoyo from '../store/contract.store';
 import ConnectButton from './ConnectButton';
 
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
+  const { getInitialData } = usePoyo();
+
+  React.useEffect(() => {
+    void getInitialData();
+  }, []);
   return (
     <header className="bg-headerFooter">
       <div className="flex justify-between items-center py-5 container mx-auto px-20">
