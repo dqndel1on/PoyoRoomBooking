52ef82d1019600147fac575559641037c29ca249
sanjipun <iamsanjipun@gmail.com>
2022-01-18 23:08:57 +0545

    day 8: update landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -12,7 +12,7 @@ const LandingPage: React.FC<LandingPageProps> = () => {
         <h1 className="text-black font-black text-8xl mt-10">
           At <span className="text-pink-600">PoYo.</span>
         </h1>
-        <div className="mt-32 max-w-7xl mx-auto border p-3 flex justify-around text-black">
+        {/*<div className="mt-32 max-w-7xl mx-auto border p-3 flex justify-around text-black">
           <select className="p-2 w-full mx-2 bg-black text-white">
             <option value="" disabled selected>
               Select Branch
@@ -33,7 +33,7 @@ const LandingPage: React.FC<LandingPageProps> = () => {
           </select>
           <input placeholder="Name" className="p-2 w-full mx-2 bg-black text-white"></input>
           <button className="p-2 bg-pink-600 w-full text-white">Book Room</button>
-        </div>
+        </div>*/}
       </div>
     </div>
   );
