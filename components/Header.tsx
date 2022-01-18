8dffc488bb2b44d334d9fb440c025c4dfedbcae0
sanjipun <iamsanjipun@gmail.com>
2022-01-18 23:08:47 +0545

    day 8 : update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import usePoyo from '../store/contract.store';
 import ConnectButton from './ConnectButton';
@@ -5,15 +6,27 @@ import ConnectButton from './ConnectButton';
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
-  const { getInitialData } = usePoyo();
+  const { getInitialData, totalBranches, getBranches } = usePoyo();
+
+  React.useEffect(() => {
+    if (totalBranches > 0) {
+      let i = 1;
+      for (i; i <= totalBranches; i++) {
+        getBranches(i);
+      }
+    }
+  }, [totalBranches]);
 
   React.useEffect(() => {
     void getInitialData();
   }, []);
+
   return (
     <header className="bg-headerFooter">
       <div className="flex justify-between items-center py-5 container mx-auto px-20">
-        <img src="/Poyo Logo.png" alt="Poyo Logo" className="h-10 w-auto" />
+        <Link href="/">
+          <img src="/Poyo Logo.png" alt="Poyo Logo" className="h-10 w-auto" />
+        </Link>
         <ConnectButton />
       </div>
     </header>
