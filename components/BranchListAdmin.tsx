e0de86b4073e353b9ee0c872b2a0c02655487b69
sanjipun <iamsanjipun@gmail.com>
2022-01-18 23:08:06 +0545

    day 8: list of branches

--- components/BranchListAdmin.tsx
+++ components/BranchListAdmin.tsx
@@ -1,24 +1,20 @@
 import React, { useEffect } from 'react';
 import usePoyo from '../store/contract.store';
+import BranchCard from './BranchCard';
 
 interface BranchListAdminProps {}
 
 const BranchListAdmin: React.FC<BranchListAdminProps> = () => {
-  const { branches, totalBranches, getBranches } = usePoyo();
+  const { branches } = usePoyo();
 
-  useEffect(() => {
-    if (totalBranches > 0) {
-      let i = 1;
-      for (i; i <= totalBranches; i++) {
-        getBranches(i);
-      }
-    }
-  }, [totalBranches]);
-
-  console.log(branches, totalBranches);
   return (
     <div className="mt-10">
       <h1 className="text-3xl font-black">List of Branches</h1>
+      <div className="flex justify-between items-center flex-wrap my-10">
+        {branches.map((branch) => (
+          <BranchCard key={branch[0]} branch={branch} />
+        ))}
+      </div>
     </div>
   );
 };
